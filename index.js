46d70da728f00c318770fde1d0cd13e0937e3dfe
Nisarg Prajapati <nisargprajapati202@gmail.com>
2024-03-02 12:35:29 -0600

    Made Changes

--- index.js
+++ index.js
@@ -1,46 +1,45 @@
 //jshint esversion: 6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const app = express();
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
   res.render("index");
 });
 
-app.post("/", function(req, res) {
-
+app.post("/", function (req, res) {
   var transporter = nodemailer.createTransport({
-    service: 'gmail',
+    service: "gmail",
     auth: {
-      user: 'nisargprajapati202@gmail.com',
-      pass: ''
-    }
+      user: "nisargprajapati202@gmail.com",
+      pass: process.env.GMAIL_PASSWORD,
+    },
   });
 
   var mailOptions = {
-    from: 'nisargprajapati202@gmail.com',
+    from: "nisargprajapati202@gmail.com",
     to: req.body.email,
-    subject: 'cctv camera store',
+    subject: "cctv camera store",
     html: '<h1>Welcome to our cctv camera store. If you want to make a website so contact me <a href="tel:+917573860509">7573860509</a></h1>',
-    text: "welcome to cctv camera store"
+    text: "welcome to cctv camera store",
   };
 
-  transporter.sendMail(mailOptions, function(error, info){
+  transporter.sendMail(mailOptions, function (error, info) {
     if (error) {
       console.log(error);
     } else {
-      console.log('Email sent: ' + info.response);
+      console.log("Email sent: " + info.response);
     }
   });
-res.redirect("/");
+  res.redirect("/");
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.listen(process.env.PORT || 3000, function () {
   console.log("server is running on port 3000.");
 });
